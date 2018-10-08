72bd04c72a563f19c67dad54f1ead14d38a31e88
John <tonmoyshuvo778@gmail.com>
2018-10-08 03:20:01 +0000

    elahi

--- script.js
+++ script.js
@@ -53,3 +53,32 @@ function getReadStatus(index){
 console.log(getReadStatus(1));
 console.log(getReadStatus(0));
 console.log(getReadStatus(2));
+
+
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+
+function addItem(newItem){
+    cart.push(newItem);
+    console.log(cart);
+}
+var newItem={
+name: 'Watch',
+price:64,
+quantity:1
+};
+addItem(newItem);
